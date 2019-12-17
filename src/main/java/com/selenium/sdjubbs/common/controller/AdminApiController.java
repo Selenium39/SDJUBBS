46dd4ee79c5d26802aa187a7af52c9df0e570e35
wantao666 <895484122@qq.com>
2019-12-17 23:12:56 +0800

    2019-12-17 23:12

--- src/main/java/com/selenium/sdjubbs/common/controller/AdminApiController.java
+++ src/main/java/com/selenium/sdjubbs/common/controller/AdminApiController.java
@@ -8,12 +8,12 @@ import com.selenium.sdjubbs.common.bean.User;
 import com.selenium.sdjubbs.common.service.UserService;
 import com.selenium.sdjubbs.common.util.Constant;
 import com.selenium.sdjubbs.common.util.Result;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -27,12 +27,12 @@ public class AdminApiController {
 
     @GetMapping(Api.USER)
     @ApiOperation(value = "获取所有的用户")
-
-    /**
-     * limit:每次查询的页数
-     * page:查询的页数
-     */
-    public Result getAllUser(String page, String limit) {
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "页数", required = true, example = "1"),
+            @ApiImplicitParam(name = "limit", value = "每页记录数", required = true, example = "10"),
+            @ApiImplicitParam(name = "order", value = "排序", required = false, example = "id asc"),
+    })
+    public Result getAllUser(String page, String limit,String order) {
         log.info("limit: " + limit + " page: " + page);
         int pageSize = 0;
         int pageNum = 0;
@@ -44,9 +44,7 @@ public class AdminApiController {
             return Result.failure(Constant.REQUEST_PARAM_FORMAT_ERROR_CODE, Constant.REQUEST_PARAM_FORMAT_ERROR);
         }
         //获取第pageNum页,pageSize条内容
-        //todo 排序
-//        PageHelper.startPage(pageNum, pageSize,"id desc");
-        PageHelper.startPage(pageNum, pageSize);
+        PageHelper.startPage(pageNum, pageSize,order);
         List<User> users = userService.getAllUser();
         PageInfo<User> pageInfo = new PageInfo<User>(users);
         if (users == null) {
